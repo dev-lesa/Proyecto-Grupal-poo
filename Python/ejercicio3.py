6f198ba91f1492403cc426ee29ae0f3534d9dd49
Luis <luiseduardosa2006@gmail.com>
2025-06-13 11:22:43 -0500

    hola

--- Python/ejercicio3.py
+++ Python/ejercicio3.py
@@ -23,9 +23,9 @@ class Laptop(Producto):
 
 
 if __name__ == "__main__":
-    tv = Televisor("Samsung", 800, 55)
+    tv = Televisor("samsung", 800, 55)
     celular = Telefono("iPhone", 1200, 1)
-    laptop = Laptop("Lenovo", 950, "16GB")
+    laptop = Laptop("Lenovo", 950, "16 GB")
 
     print(tv.nombre)
     print(f"Precio con IVA: {tv.precio_final()}", "\n")
